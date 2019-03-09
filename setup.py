d8c76c7f9f226cf33f85d9374c1a85c43f4d1269
Matt Gruskin <matthew.gruskin@gmail.com>
2019-03-09 10:30:55 -0500

    Include README.md description in package.

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
 setup(
     name="grilops",
-    version="0.1",
+    version="0.1.1",
     description="GRId LOgic Puzzle Solver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/obijywk/grilops",
     author="Matt Gruskin",
     author_email="matthew.gruskin@gmail.com",
