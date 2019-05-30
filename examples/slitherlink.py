f1095231d7f032005a52925f3ebfda116e80599b
Matt Gruskin <matthew.gruskin@gmail.com>
2019-05-30 08:09:32 -0400

    Add a regions-based version of the Slitherlink example.

--- examples/slitherlink.py
+++ examples/slitherlink.py
@@ -1,9 +1,10 @@
 """Slitherlink solver example."""
 
-from z3 import If, Sum
+from z3 import If, Int, Or, Sum
 
 import grilops
 import grilops.loops
+import grilops.regions
 
 
 HEIGHT, WIDTH = 6, 6
@@ -23,8 +24,8 @@ GIVENS = {
 }
 
 
-def main():
-  """Slitherlink solver example."""
+def loop_solve():
+  """Slitherlink solver example using loops."""
   sym = grilops.loops.LoopSymbolSet()
   sym.append("EMPTY", " ")
 
@@ -66,5 +67,60 @@ def main():
     print("No solution")
 
 
+def region_solve():
+  """Slitherlink solver example using regions."""
+  sym = grilops.SymbolSet([("I", chr(0x2588)), ("O", " ")])
+  sg = grilops.SymbolGrid(HEIGHT, WIDTH, sym)
+  rc = grilops.regions.RegionConstrainer(
+      HEIGHT, WIDTH, solver=sg.solver, complete=False)
+
+  region_id = Int("region_id")
+  for y in range(HEIGHT):
+    for x in range(WIDTH):
+      # Each cell must be either "inside" (part of a single region) or
+      # "outside" (not part of any region).
+      sg.solver.add(
+          Or(
+              rc.region_id_grid[y][x] == region_id,
+              rc.region_id_grid[y][x] == -1
+          )
+      )
+      sg.solver.add(
+          (sg.grid[y][x] == sym.I) == (rc.region_id_grid[y][x] == region_id))
+
+      if (y, x) not in GIVENS:
+        continue
+      given = GIVENS[(y, x)]
+      neighbors = sg.adjacent_cells(y, x)
+      # The number of grid edge border segments adjacent to this cell.
+      num_grid_borders = 4 - len(neighbors)
+      # The number of adjacent cells on the opposite side of the loop line.
+      num_different_neighbors = sum(
+          If(n.symbol != sg.grid[y][x], 1, 0) for n in neighbors
+      )
+      # If this is an "inside" cell, we should count grid edge borders as loop
+      # segments, but if this is an "outside" cell, we should not.
+      sg.solver.add(
+          If(
+              sg.grid[y][x] == sym.I,
+              given == num_different_neighbors + num_grid_borders,
+              given == num_different_neighbors
+          )
+      )
+
+  if sg.solve():
+    sg.print()
+    print()
+    if sg.is_unique():
+      print("Unique solution")
+    else:
+      print("Alternate solution")
+      sg.print()
+  else:
+    print("No solution")
+
+
 if __name__ == "__main__":
-  main()
+  loop_solve()
+  print()
+  region_solve()
