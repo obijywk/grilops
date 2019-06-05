0d3b5162d000827597d4f08a5eb44cc015a5f272
Matt Gruskin <matthew.gruskin@gmail.com>
2019-06-05 10:29:14 -0400

    Bump version number to 0.2.1.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="grilops",
-    version="0.2.0",
+    version="0.2.1",
     description="GRId LOgic Puzzle Solver",
     long_description=long_description,
     long_description_content_type="text/markdown",
