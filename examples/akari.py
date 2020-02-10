95d69c68b79f4f1d883db07526ee5bbb702cb059
Matt Gruskin <matthew.gruskin@gmail.com>
2020-02-10 09:58:29 -0500

    Revise Akari example to leverage Lattice

--- examples/akari.py
+++ examples/akari.py
@@ -3,17 +3,17 @@
 Example puzzle can be found at https://en.wikipedia.org/wiki/Light_Up_(puzzle).
 """
 
-import sys
 from z3 import If, PbEq
 
 import grilops
 import grilops.sightlines
-from grilops.geometry import Point
 
 
 def main():
   """Akari solver example."""
   size = 10
+  lattice = grilops.get_square_lattice(size)
+
   black_cells = {
       (0, 0): None,
       (0, 3): None,
@@ -41,17 +41,14 @@ def main():
       (9, 9): 0,
   }
 
-  for y in range(size):
-    for x in range(size):
-      if (y, x) in black_cells:
-        v = black_cells[(y, x)]
-        if v is None:
-          sys.stdout.write(chr(0x2588))
-        else:
-          sys.stdout.write(str(v))
-      else:
-        sys.stdout.write(" ")
-    print()
+  def print_given(point):
+    if point in black_cells:
+      v = black_cells.get(point)
+      if v is None:
+        return chr(0x2588)
+      return str(v)
+    return None
+  lattice.print(print_given)
   print()
 
   sym = grilops.SymbolSet([
@@ -59,48 +56,43 @@ def main():
       ("EMPTY", " "),
       ("LIGHT", "*"),
   ])
-  lattice = grilops.get_square_lattice(size)
   sg = grilops.SymbolGrid(lattice, sym)
 
-  for y in range(size):
-    for x in range(size):
-      p = Point(y, x)
-      if (y, x) in black_cells:
-        sg.solver.add(sg.cell_is(p, sym.BLACK))
-        light_count = black_cells[p]
-        if light_count is not None:
-          sg.solver.add(PbEq(
-              [(n.symbol == sym.LIGHT, 1) for n in sg.edge_sharing_neighbors(p)],
-              light_count
-          ))
-      else:
-        # All black cells are given; don't allow this cell to be black.
-        sg.solver.add(sg.cell_is_one_of(p, [sym.EMPTY, sym.LIGHT]))
+  for point in lattice.points:
+    if point in black_cells:
+      sg.solver.add(sg.cell_is(point, sym.BLACK))
+      light_count = black_cells[point]
+      if light_count is not None:
+        sg.solver.add(PbEq(
+            [(n.symbol == sym.LIGHT, 1) for n in sg.edge_sharing_neighbors(point)],
+            light_count
+        ))
+    else:
+      # All black cells are given; don't allow this cell to be black.
+      sg.solver.add(sg.cell_is_one_of(point, [sym.EMPTY, sym.LIGHT]))
 
   def is_black(c):
     return c == sym.BLACK
   def count_light(c):
     return If(c == sym.LIGHT, 1, 0)
 
-  for y in range(size):
-    for x in range(size):
-      if (y, x) in black_cells:
-        continue
-      p = Point(y, x)
-      visible_light_count = sum(
-          grilops.sightlines.count_cells(
-              sg, n.location, n.direction, count=count_light, stop=is_black
-          ) for n in sg.edge_sharing_neighbors(p)
-      )
-      # Ensure that each light cannot see any other lights, and that each cell
-      # is lit by at least one light.
-      sg.solver.add(
-          If(
-              sg.cell_is(p, sym.LIGHT),
-              visible_light_count == 0,
-              visible_light_count > 0
-          )
-      )
+  for point in lattice.points:
+    if point in black_cells:
+      continue
+    visible_light_count = sum(
+        grilops.sightlines.count_cells(
+            sg, n.location, n.direction, count=count_light, stop=is_black
+        ) for n in sg.edge_sharing_neighbors(point)
+    )
+    # Ensure that each light cannot see any other lights, and that each cell
+    # is lit by at least one light.
+    sg.solver.add(
+        If(
+            sg.cell_is(point, sym.LIGHT),
+            visible_light_count == 0,
+            visible_light_count > 0
+        )
+    )
 
   if sg.solve():
     sg.print()
