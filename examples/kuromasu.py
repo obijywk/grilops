64f159182f25ccfe8d8e7e03309379c0a119c967
Matt Gruskin <matthew.gruskin@gmail.com>
2020-02-10 20:57:11 -0500

    Revise Kuromasu example to better leverage geometry.

--- examples/kuromasu.py
+++ examples/kuromasu.py
@@ -13,24 +13,24 @@ from grilops.geometry import Point
 
 HEIGHT, WIDTH = 11, 11
 GIVENS = {
-    (0, 2): 9,
-    (0, 8): 8,
-    (1, 8): 7,
-    (2, 4): 12,
-    (2, 10): 16,
-    (3, 0): 9,
-    (4, 1): 10,
-    (5, 2): 12,
-    (5, 4): 8,
-    (5, 6): 11,
-    (5, 8): 3,
-    (6, 9): 3,
-    (7, 10): 3,
-    (8, 0): 7,
-    (8, 6): 2,
-    (9, 2): 7,
-    (10, 2): 2,
-    (10, 8): 5,
+    Point(0, 2): 9,
+    Point(0, 8): 8,
+    Point(1, 8): 7,
+    Point(2, 4): 12,
+    Point(2, 10): 16,
+    Point(3, 0): 9,
+    Point(4, 1): 10,
+    Point(5, 2): 12,
+    Point(5, 4): 8,
+    Point(5, 6): 11,
+    Point(5, 8): 3,
+    Point(6, 9): 3,
+    Point(7, 10): 3,
+    Point(8, 0): 7,
+    Point(8, 6): 2,
+    Point(9, 2): 7,
+    Point(10, 2): 2,
+    Point(10, 8): 5,
 }
 
 
@@ -42,8 +42,7 @@ def main():
   rc = grilops.regions.RegionConstrainer(
       lattice, solver=sg.solver, complete=False)
 
-  for (y, x), c in GIVENS.items():
-    p = Point(y, x)
+  for p, c in GIVENS.items():
     # Numbered cells may not be black.
     sg.solver.add(sg.cell_is(p, sym.W))
 
@@ -65,27 +64,24 @@ def main():
   white_root = min(GIVENS.keys())
   white_region_id = lattice.point_to_index(white_root)
 
-  for y in range(HEIGHT):
-    for x in range(WIDTH):
-      p = Point(y, x)
+  for p in lattice.points:
+    # No two black cells may be horizontally or vertically adjacent.
+    sg.solver.add(
+        Implies(
+            sg.cell_is(p, sym.B),
+            And(*[n.symbol == sym.W for n in sg.edge_sharing_neighbors(p)])
+        )
+    )
 
-      # No two black cells may be horizontally or vertically adjacent.
-      sg.solver.add(
-          Implies(
-              sg.cell_is(p, sym.B),
-              And(*[n.symbol == sym.W for n in sg.edge_sharing_neighbors(p)])
-          )
-      )
-
-      # All white cells must have the same region ID. All black cells must not
-      # be part of a region.
-      sg.solver.add(
-          If(
-              sg.cell_is(p, sym.W),
-              rc.region_id_grid[p] == white_region_id,
-              rc.region_id_grid[p] == -1
-          )
-      )
+    # All white cells must have the same region ID. All black cells must not
+    # be part of a region.
+    sg.solver.add(
+        If(
+            sg.cell_is(p, sym.W),
+            rc.region_id_grid[p] == white_region_id,
+            rc.region_id_grid[p] == -1
+        )
+    )
 
   def print_grid():
     sg.print(
