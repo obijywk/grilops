a0840e2ccc3638e4724588dc2b453522ffdac581
Matt Gruskin <matthew.gruskin@gmail.com>
2020-02-10 20:36:04 -0500

    Revise Fillomino example to better leverage geometry

--- examples/fillomino.py
+++ examples/fillomino.py
@@ -7,7 +7,6 @@ from z3 import Implies  # type: ignore
 
 import grilops
 import grilops.regions
-from grilops.geometry import Point
 
 
 def main():
@@ -32,32 +31,29 @@ def main():
       solver=sg.solver
   )
 
-  for y in range(len(givens)):
-    for x in range(len(givens[0])):
-      p = Point(y, x)
+  for p in lattice.points:
+    # The filled number must match the size of the region.
+    sg.solver.add(sg.grid[p] == rc.region_size_grid[p])
 
-      # The filled number must match the size of the region.
-      sg.solver.add(sg.grid[p] == rc.region_size_grid[p])
+    # The size of the region must match the clue.
+    given = givens[p.y][p.x]
+    if given != 0:
+      sg.solver.add(rc.region_size_grid[p] == given)
 
-      # The size of the region must match the clue.
-      given = givens[y][x]
-      if given != 0:
-        sg.solver.add(rc.region_size_grid[p] == given)
-
-      # Different regions of the same size may not be orthogonally adjacent.
-      region_sizes = [
-          n.symbol for n in lattice.edge_sharing_neighbors(rc.region_size_grid, p)
-      ]
-      region_ids = [
-          n.symbol for n in lattice.edge_sharing_neighbors(rc.region_id_grid, p)
-      ]
-      for region_size, region_id in zip(region_sizes, region_ids):
-        sg.solver.add(
-            Implies(
-                rc.region_size_grid[p] == region_size,
-                rc.region_id_grid[p] == region_id
-            )
-        )
+    # Different regions of the same size may not be orthogonally adjacent.
+    region_sizes = [
+        n.symbol for n in lattice.edge_sharing_neighbors(rc.region_size_grid, p)
+    ]
+    region_ids = [
+        n.symbol for n in lattice.edge_sharing_neighbors(rc.region_id_grid, p)
+    ]
+    for region_size, region_id in zip(region_sizes, region_ids):
+      sg.solver.add(
+          Implies(
+              rc.region_size_grid[p] == region_size,
+              rc.region_id_grid[p] == region_id
+          )
+      )
 
   if sg.solve():
     sg.print()
