0005a19c0adbb5a6f24fc039bd8ccb69013fc0a0
Matt Gruskin <matthew.gruskin@gmail.com>
2021-12-13 00:47:58 -0500

    Add Aquarium example

---
+++ examples/aquarium.py
@@ -0,0 +1,63 @@
+"""Aquarium solver example."""
+
+from z3 import PbEq
+
+import grilops
+
+HEIGHT, WIDTH = 6, 6
+
+REGIONS = [
+  "ABBBCC",
+  "AADDCC",
+  "AEFFFC",
+  "EEEECC",
+  "EEEECC",
+  "ECCCCC",
+]
+
+COL_CLUES = [3, 3, 4, 4, 4, 3]
+ROW_CLUES = [4, 4, 4, 2, 2, 5]
+
+def main():
+  """Aquarium solver example."""
+  sym = grilops.SymbolSet([("B", chr(0x2588)), ("W", " ")])
+  lattice = grilops.get_rectangle_lattice(HEIGHT, WIDTH)
+  sg = grilops.SymbolGrid(lattice, sym)
+
+  # Number of shaded cells per row / column must match clues.
+  for y in range(HEIGHT):
+    sg.solver.add(
+      PbEq(
+        [(sg.grid[(y, x)] == sym.B, 1) for x in range(WIDTH)],
+        ROW_CLUES[y]
+      )
+    )
+  for x in range(WIDTH):
+    sg.solver.add(
+      PbEq(
+        [(sg.grid[(y, x)] == sym.B, 1) for y in range(HEIGHT)],
+        COL_CLUES[x]
+      )
+    )
+
+  # The water level in each aquarium is the same across its full width.
+  for y in range(HEIGHT):
+    for al in set(REGIONS[y]):
+      cells = [sg.grid[(y, x)] for x in range(WIDTH) if REGIONS[y][x] == al]
+      for cell in cells[1:]:
+        sg.solver.add(cell == cells[0])
+
+  if sg.solve():
+    sg.print()
+    print()
+    if sg.is_unique():
+      print("Unique solution")
+    else:
+      print("Alternate solution")
+      sg.print()
+  else:
+    print("No solution")
+
+
+if __name__ == "__main__":
+  main()
